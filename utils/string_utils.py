75a69dc02c0d860ab30794f1879fe626c49e855e
Fernando Lanfranchi <felan@havana.localdomain>
2025-08-18 11:50:21 -0300

    removing outdated code

--- utils/string_utils.py
+++
@@ -1,8 +0,0 @@
-# *-* coding: utf-8 *-*
-
-def trim_string(s):
-    """This function removes not just leading and trailing whitespace, but also whitespaces between words, keeping only single spaces."""
-    if isinstance(s, str):
-        return ' '.join(s.split())
-    else:
-        return s
