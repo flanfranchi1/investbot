55b732bdf6b808ebb6e0d159abbaabc4df00dcd5
Fernando Lanfranchi <felan@havana.localdomain>
2025-09-01 16:32:58 -0300

    ~removing outdated code not used anymore

--- database.py
+++ database.py
@@ -3,6 +3,7 @@
 import sqlalchemy as db
 import logging
 
+
 def get_engine() -> db.Engine:
     """Creates a database engine instance."""
     engine = db.create_engine('sqlite:///data/sqlite/stock_data.db')
@@ -23,7 +24,8 @@ def create_sp500_table(engine):
              db.Column('cik', db.String, nullable=True),
              db.Column('founded', db.Integer, nullable=True),
              db.Column('registry_date', db.Date, nullable=False),
-             db.UniqueConstraint('symbol', 'data_added', name='uix_symbol_date')
+             db.UniqueConstraint('symbol', 'data_added',
+                                 name='uix_symbol_date')
              )
     metadata.create_all(engine)
     logging.info("Table 'sp500_companies' is ready.")
@@ -48,26 +50,3 @@ def create_price_table(engine: db.Engine) -> None:
 
     metadata.create_all(engine)
     logging.info("Table 'stock_prices' is ready.")
-
-def save_data_to_db(data: dict, ticker: str, engine: db.Engine) -> None:
-    """Parses and saves stock data into the database."""
-    with engine as connection:
-        metadata = db.MetaData()
-        stock_prices = db.Table('stock_prices', metadata, autoload_with=connection)
-
-        for date, daily_data in data['Time Series (Daily)'].items():
-            try:
-                insert_query = stock_prices.insert().values(
-                    ticker=ticker,
-                    price_date=datetime.strptime(date, '%Y-%m-%d').date(),
-                    open=float(daily_data['1. open']),
-                    high=float(daily_data['2. high']),
-                    low=float(daily_data['3. low']),
-                    close=float(daily_data['4. close']),
-                    volume=int(daily_data['5. volume'])
-                )
-                connection.execute(insert_query)
-            except IntegrityError as e:
-                logging.warning(f"Duplicate entry for {ticker} on {date}: {e}")
-            except Exception as e:
-                logging.error(f"Error saving data for {ticker} on {date}: {e}")
